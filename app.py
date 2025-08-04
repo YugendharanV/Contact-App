8a1208755aa7c85c8cfef3c1e8bc82b5dad8f58c
demo <s@gmail.com>
2025-08-04 19:50:37 +0530

    changed

--- app.py
+++ app.py
@@ -21,4 +21,6 @@ def delete_contact(index):
     return redirect('/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
